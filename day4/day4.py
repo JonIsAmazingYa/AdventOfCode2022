95170a1a2f91f3777d469682ae0f55ad3a24f581
jmounty <jmounty@cisco.com>
2022-12-05 13:07:18 -0800

    Add day4 part 2

--- day4/day4.py
+++ day4/day4.py
@@ -5,8 +5,16 @@ class CleanupSegment:
       return True
     return False
 
+  def overlaps_with(self, other):
+    if self.start_segment <= other.start_segment and self.end_segment >= other.start_segment or \
+      self.start_segment <= other.end_segment and self.end_segment >= other.end_segment:
+      return True
+    return False
+
   def __init__(self, input):
     self.start_segment, self.end_segment = input.split('-')
+    self.start_segment = int(self.start_segment)
+    self.end_segment = int(self.end_segment)
     if self.start_segment > self.end_segment:
       tmp = self.end_segment
       self.end_segment = self.start_segment
@@ -14,10 +22,10 @@ class CleanupSegment:
 
 acc = 0
 
-with open('sample.txt') as fh:
+with open('input.txt') as fh:
   for line in fh.readlines():
     first, second = [CleanupSegment(input) for input in line.strip().split(',')]
 
-    acc += first.contains(second) or second.contains(first)
+    acc += first.overlaps_with(second) or second.overlaps_with(first)
 
 print(acc)
