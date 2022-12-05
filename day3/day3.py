0fd22cc9a7398e225ddc9ca3334473f8f79deb6a
jmounty <jmounty@cisco.com>
2022-12-05 10:03:38 -0500

    Add completed day3

--- day3/day3.py
+++ day3/day3.py
@@ -1,14 +1,18 @@
-priorities = {v: k for k,v in enumerate(char for char in '0abcdefghijklmnopqrstuvwxyzABCEDFGHIJKLMNOPQRSTUVWXYZ')}
+priorities = {v: k for k,v in enumerate(char for char in '0abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')}
 
 acc = 0
-with open('sample.txt') as fh:
-  for line in fh.readlines():
-    result = [char for char in line[:int(len(line)/2)] if char in line[int(len(line)/2):]][0]
+with open('input.txt') as fh:
+  line = fh.readline()
 
-    print(result)
+  while line:
+    line1 = line
+    line2 = fh.readline()
+    line3 = fh.readline()
 
-    print(priorities[result])
+    result = [char for char in line1 if char in line2 and char in line3][0]
 
     acc += priorities[result]
+
+    line = fh.readline()
 
 print(acc)
